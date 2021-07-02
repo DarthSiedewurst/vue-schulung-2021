4fe7f978d865ec6b8daf2cc5e578198d07603800
Marco Mueller <marco.1.mueller@zurich.com>
2021-07-02 12:53:58 +0200

    init

---
+++ vue-schulung-2021/src/views/Marco.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    Marco
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Marco",
+  components: {
+  },
+});
+</script>
