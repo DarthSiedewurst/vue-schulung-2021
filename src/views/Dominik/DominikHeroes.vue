1be08d948565b1e099c8c39946ad040500830945
DOMINIK.ODEKERKEN <dominik.odekerken@zurich.com>
2021-07-06 13:50:44 +0200

    Dominik - Heroes and Villains hinzugefügt

---
+++ src/views/Dominik/DominikHeroes.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1>Helden</h1>
+    <div class="row">
+      <div class="col-4"></div>
+      <div class="col-8"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive } from "vue";
+
+export default defineComponent({
+  name: "MarcoHeroes",
+  components: {},
+  props: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+<style scoped></style>
